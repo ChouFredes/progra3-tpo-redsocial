9801b8f19c83baafbfca7f936e61a272ef3829e3
FionaPardo <110065793+FionaPardo@users.noreply.github.com>
2025-11-14 00:48:29 -0300

    Refactor DivideAndConquerSorts.java

--- src/main/java/com/tpo/redsocial/algorithms/DivideAndConquerSorts.java
+++ src/main/java/com/tpo/redsocial/algorithms/DivideAndConquerSorts.java
@@ -1,30 +1,58 @@
-// src/main/java/com/tpo/redsocial/algorithms/DivideAndConquerSorts.java
 package com.tpo.redsocial.algorithms;
-import java.util.*;
+
+import java.util.Arrays;
 
 public class DivideAndConquerSorts {
-    public static void quickSort(double[] a, int l, int r){
-        if (l>=r) return;
-        double p=a[(l+r)/2];
-        int i=l,j=r;
-        while(i<=j){
-            while(a[i]<p) i++;
-            while(a[j]>p) j--;
-            if (i<=j){ double t=a[i]; a[i]=a[j]; a[j]=t; i++; j--; }
+
+    public static void quickSort(double[] a) {
+        if (a == null || a.length < 2) return;
+        quick(a, 0, a.length - 1);
+    }
+
+    private static void quick(double[] a, int l, int r) {
+        int i = l;
+        int j = r;
+        double pivot = a[(l + r) / 2];
+
+        while (i <= j) {
+            while (a[i] < pivot) i++;
+            while (a[j] > pivot) j--;
+
+            if (i <= j) {
+                double tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+                i++;
+                j--;
+            }
         }
-        if (l<j) quickSort(a,l,j);
-        if (i<r) quickSort(a,i,r);
+
+        if (l < j) quick(a, l, j);
+        if (i < r) quick(a, i, r);
     }
-    public static double[] mergeSort(double[] a){
-        if (a.length<=1) return a;
-        int m=a.length/2;
-        return merge(mergeSort(Arrays.copyOfRange(a,0,m)),
-                     mergeSort(Arrays.copyOfRange(a,m,a.length)));
+
+    public static double[] mergeSort(double[] a) {
+        if (a == null || a.length <= 1) return a;
+        int mid = a.length / 2;
+
+        double[] left = mergeSort(Arrays.copyOfRange(a, 0, mid));
+        double[] right = mergeSort(Arrays.copyOfRange(a, mid, a.length));
+
+        return merge(left, right);
     }
-    private static double[] merge(double[] L,double[] R){
-        double[] res=new double[L.length+R.length]; int i=0,j=0,k=0;
-        while(i<L.length && j<R.length) res[k++]= (L[i]<=R[j])? L[i++]:R[j++];
-        while(i<L.length) res[k++]=L[i++]; while(j<R.length) res[k++]=R[j++];
+
+    private static double[] merge(double[] l, double[] r) {
+        double[] res = new double[l.length + r.length];
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < l.length && j < r.length) {
+            res[k++] = (l[i] <= r[j]) ? l[i++] : r[j++];
+        }
+
+        while (i < l.length) res[k++] = l[i++];
+        while (j < r.length) res[k++] = r[j++];
+
         return res;
     }
 }
